1657f4901dc53806a74a9a9492926e5e7f52453a
Paola Duquino <angela.duquino@appgate.com>
2020-07-02 18:04:22 -0500

    Create project

--- automationCucumberishCucumberTests/steps/Background.swift
+++ automationCucumberishCucumberTests/steps/Background.swift
@@ -4,12 +4,6 @@ import Cucumberish
 class Background {
     
     func backgroundSteps(){
-        
-       
-        Given("a Mock") { (args, userInfo) -> Void in
-            
-            print("AUTOMATION - Set the authentication factors with: \(factorList!)")
-        }
 
         Given("a Mock with the params:") { (args, userInfo) -> Void in
             let params  = (userInfo!["DataTable"] as! NSArray);
